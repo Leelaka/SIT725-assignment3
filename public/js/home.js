091373e68c2228ef3266193decef21b77ac83d23
Leelaka Adassuriya <37642488+Leelaka@users.noreply.github.com>
2020-10-19 14:14:16 +1100

    rename pages

--- public/js/home.js
+++ public/js/home.js
@@ -1,7 +1,22 @@
 const logout=()=>{
-    window.location.replace('/index.html')
-}
+    window.location.replace('../index.html');
+};
+
+const play=()=>{
+    window.location.replace('./lobby.html');
+};
+
+const friends=()=>{
+    window.location.replace('./friends.html');
+};
+
+const settings=() => {
+    window.location.replace('./settings.html');
+};
 
 $(document).ready(function(){
-    console.log('Home page ready')
-})
+    console.log('Home page ready');
+
+});
+
+
---
+++ public/js/lobby.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    console.log('Queue page loaded up');
+});
+
+const back=()=>{
+    window.location.replace('./home.html');
+};
